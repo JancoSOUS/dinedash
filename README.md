d690c50b3a63d256a28b5c9338667395bf665377
JancoSOUS <jancobuitendag@gmail.com>
2023-04-04 00:50:49 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dinedash
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dinedash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dinedash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
